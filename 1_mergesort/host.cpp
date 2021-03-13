c6dfd2ca6ba580f659e55d0d799b8f7686e6630a
Jan S Kowalski <janstefankowalski@gmail.com>
2021-03-13 17:38:40 -0500

    Bitronic mergesort kernel works in simulations

---
+++ 1_mergesort/host.cpp
@@ -0,0 +1,192 @@
+/**
+* Copyright (C) 2020 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
+#include "xcl2.hpp"
+#include <algorithm>
+#include <vector>
+#include <time.h>
+
+//Make sure this information matches the kernel!
+#define DATA_SIZE 4096
+#define KERNEL_PRECISION 4	//2, 4 or 8
+
+//void mergesort(int, int, void*);
+void mergesort(int, int, std::vector<int, aligned_allocator<int>>&);
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	clock_t start, end;
+
+	//start timer
+	start = clock();
+
+	//******************* Declare variables ******************************
+
+	std::string binaryFile = argv[1];
+	size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
+
+	std::vector<int, aligned_allocator<int> > source_values(DATA_SIZE);
+	std::vector<int, aligned_allocator<int> > source_hw_results(DATA_SIZE);
+	std::vector<int, aligned_allocator<int> > source_sw_results(DATA_SIZE);
+
+	cl_int err;
+	cl::Kernel krnl_mergesort;
+	cl::CommandQueue q;
+	cl::Context context;
+
+	//******************* Generate data ******************************
+
+	std::generate(source_values.begin(), source_values.end(), std::rand);
+
+	for (int i = 0; i < DATA_SIZE; i++) {
+		source_sw_results[i] = source_values[i];
+		source_hw_results[i] = 0;
+	}
+
+	std::sort(source_sw_results.begin(), source_sw_results.end());
+
+
+//	for (int i = 0; i < DATA_SIZE; i+=2) {
+//		if (source_sw_results[i] > source_sw_results[i+1]){
+//			source_sw_results[i] = source_values[i+1];
+//			source_sw_results[i+1] = source_values[i];
+//		}
+//	}
+
+	//******************* Try to access platform ************************
+
+	auto devices = xcl::get_xil_devices();
+	auto fileBuf = xcl::read_binary_file(binaryFile);
+	cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
+	bool valid_device = false;
+	for (unsigned int i = 0; i < devices.size(); i++) {
+		auto device = devices[i];
+		OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
+		OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+		std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+		cl::Program program(context, {device}, bins, NULL, &err);
+		if (err != CL_SUCCESS) {
+			std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
+		} else {
+			std::cout << "Device[" << i << "]: program successful!\n";
+			OCL_CHECK(err, krnl_mergesort = cl::Kernel(program, "mergesort", &err));
+			valid_device = true;
+			break;
+		}
+	}
+	if (!valid_device) {
+		std::cout << "Failed to program any device found, exit!\n";
+		exit(EXIT_FAILURE);
+	}
+
+
+	//************************  Inteface with kernel ********************
+
+	OCL_CHECK(err, cl::Buffer buffer_input(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes, source_values.data(), &err));
+	OCL_CHECK(err, cl::Buffer buffer_output(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, vector_size_bytes, source_hw_results.data(), &err));
+
+	//send arguments to hls function
+	int size = DATA_SIZE;
+	OCL_CHECK(err, err = krnl_mergesort.setArg(0, buffer_input));
+	OCL_CHECK(err, err = krnl_mergesort.setArg(1, buffer_output));
+	OCL_CHECK(err, err = krnl_mergesort.setArg(2, size));
+
+	//send data to hls function
+	OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_input}, 0));
+	//run function
+	OCL_CHECK(err, err = q.enqueueTask(krnl_mergesort));
+	//load results
+	OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output}, CL_MIGRATE_MEM_OBJECT_HOST));
+	q.finish();
+
+	//************************  Run mergesort ****************************
+
+
+	//assumes DATA_SIZE is a multiple of 2
+	mergesort(0, DATA_SIZE-1, source_hw_results);
+
+
+	//************************  Check results ****************************
+
+	std::cout << "Ran " << KERNEL_PRECISION << "-wise bitonic mergesort" << std::endl;
+	bool match = true;
+	for (int i = 0; i < DATA_SIZE; i++) {
+		if (source_hw_results[i] != source_sw_results[i]) {
+			std::cout << "Error: Result mismatch" << std::endl;
+			std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
+			      << " Device result = " << source_hw_results[i] << std::endl;
+			match = false;
+			break;
+		}
+	}
+
+	//stop timer
+	end = clock();
+	printf("Runtime: %.04f\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+
+	std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl;
+	return (match ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
+//run mergesort with a partially sorted list
+//arguments: first accessable index, last accessable index, pointer to array
+	//assumes DATA_SIZE is a multiple of 2
+//void mergesort(int begin, int end, void* array){
+void mergesort(int begin, int end, std::vector<int, aligned_allocator<int>>& array){
+
+	//kernel should have sorted n-wise groups
+	if(end-begin<KERNEL_PRECISION) return;
+
+	mergesort(begin, begin+(end-begin)/2, array);
+	mergesort(begin+(end-begin)/2+1, end, array);
+
+
+	//sort the rest
+	int i, j, a, b, start_of_b;
+	a=begin;
+	b=begin+(end-begin)/2+1;
+	start_of_b = b;
+
+	int sorted[end-begin+1];
+
+	int a_saturated = 0;
+	int b_saturated = 0;
+
+	//sort subarray
+	for(i=0; i<end-begin+1; i++){
+
+		if (a_saturated) {sorted[i] = array[b++]; continue;}
+		if (b_saturated) {sorted[i] = array[a++]; continue;}
+
+		sorted[i] = (array[a] > array[b]) ? array[b++] : array[a++];
+
+		if (a >= start_of_b) a_saturated=1;
+		if (b > end) b_saturated=1;
+	}
+
+	i=0;
+	//replace original with sorted
+	for(j=begin; j<end+1; j++){
+		array[j] = sorted[i++];
+	}
+
+}
+
---
+++ 1_mergesort/mergesort.cpp
@@ -0,0 +1,157 @@
+/**
+* Copyright (C) 2020 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
+
+//Modified to mergesort bitonic hardware
+// https://en.wikipedia.org/wiki/Bitonic_sorter
+//Jan S Kowalski 3/8/21
+
+#define BUFFER_SIZE 1024
+#define DATA_SIZE 4096
+#define KERNEL_PRECISION 4	//2, 4 or 8
+
+const unsigned int c_len = DATA_SIZE / BUFFER_SIZE;
+const unsigned int c_size = BUFFER_SIZE;
+
+extern "C" {
+
+void compare_2(int, int, unsigned int*, unsigned int*);
+
+//compare every pair of numbers, rearrange as necessary
+void mergesort(	const unsigned int* input,
+         		unsigned int* output,
+          		int size        	){
+
+
+
+	unsigned int vin_buffer[BUFFER_SIZE];
+	unsigned int vout_buffer[BUFFER_SIZE];
+
+//#if KERNEL_PRECISION==4 || KERNEL_PRECISION==8
+	unsigned int intermediate_a[BUFFER_SIZE];
+	unsigned int intermediate_b[BUFFER_SIZE];
+//#endif
+//#if KERNEL_PRECISION==8
+	unsigned int intermediate_c[BUFFER_SIZE];
+	unsigned int intermediate_d[BUFFER_SIZE];
+	unsigned int intermediate_e[BUFFER_SIZE];
+//#endif
+
+
+    	for (int i = 0; i < size; i += BUFFER_SIZE) {
+#pragma HLS LOOP_TRIPCOUNT min = c_len max = c_len
+		int chunk_size = BUFFER_SIZE;
+
+	        // not sure
+		if ((i + BUFFER_SIZE) > size) chunk_size = size - i;
+
+	read1:
+		for (int j = 0; j < chunk_size; j++) {
+	#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+	#pragma HLS PIPELINE II = 1
+		            vin_buffer[j] = input[i + j];
+		        }
+//Every pair of inputs is ordered correctly
+#if KERNEL_PRECISION==2
+	mergesort_2:
+		for(int j=0; j < chunk_size; j+=2){
+	#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+	#pragma HLS PIPELINE II = 1
+			compare_2(j, j+1, vin_buffer, vout_buffer);
+		}
+#endif
+
+
+//Every group of four inputs is ordered correctly
+#if KERNEL_PRECISION==4
+	mergesort_4:
+		for(int j=0; j < chunk_size; j+=4){
+	#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+	#pragma HLS PIPELINE II = 1
+			//layer 1
+			compare_2(j, j+1, vin_buffer, intermediate_a);
+			compare_2(j+3, j+2, vin_buffer, intermediate_a);
+			//layer 2
+			compare_2(j, j+2, intermediate_a, intermediate_b);
+			compare_2(j+1, j+3, intermediate_a, intermediate_b);
+			//layer 3
+			compare_2(j, j+1, intermediate_b, vout_buffer);
+			compare_2(j+2, j+3, intermediate_b, vout_buffer);
+		}
+#endif
+
+//Every group of eight inputs is ordered correctly
+#if KERNEL_PRECISION==8
+	mergesort_8:
+		for(int j=0; j < chunk_size; j+=8){
+	#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+	#pragma HLS PIPELINE II = 1
+			//layer 1
+			compare_2(j, j+1, vin_buffer, intermediate_a);
+			compare_2(j+3, j+2, vin_buffer, intermediate_a);
+			compare_2(j+4, j+5, vin_buffer, intermediate_a);
+			compare_2(j+7, j+6, vin_buffer, intermediate_a);
+			//layer 2
+			compare_2(j, j+2, intermediate_a, intermediate_b);
+			compare_2(j+1, j+3, intermediate_a, intermediate_b);
+			compare_2(j+7, j+5, intermediate_a, intermediate_b);
+			compare_2(j+6, j+4, intermediate_a, intermediate_b);
+			//layer 3
+			compare_2(j, j+1, intermediate_b, intermediate_c);
+			compare_2(j+2, j+3, intermediate_b, intermediate_c);
+			compare_2(j+7, j+6, intermediate_b, intermediate_c);
+			compare_2(j+5, j+4, intermediate_b, intermediate_c);
+			//layer 4
+			compare_2(j, j+4, intermediate_c, intermediate_d);
+			compare_2(j+1, j+5, intermediate_c, intermediate_d);
+			compare_2(j+2, j+6, intermediate_c, intermediate_d);
+			compare_2(j+3, j+7, intermediate_c, intermediate_d);
+			//layer 5
+			compare_2(j, j+2, intermediate_d, intermediate_e);
+			compare_2(j+1, j+3, intermediate_d, intermediate_e);
+			compare_2(j+4, j+6, intermediate_d, intermediate_e);
+			compare_2(j+5, j+7, intermediate_d, intermediate_e);
+			//layer 6
+			compare_2(j, j+1, intermediate_e, vout_buffer);
+			compare_2(j+2, j+3, intermediate_e, vout_buffer);
+			compare_2(j+4, j+5, intermediate_e, vout_buffer);
+			compare_2(j+6, j+7, intermediate_e, vout_buffer);
+
+		}
+#endif
+
+	// burst write the result
+	write:
+ 		for (int j = 0; j < chunk_size; j++) {
+	#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+	#pragma HLS PIPELINE II = 1
+			output[i + j] = vout_buffer[j];
+		}
+	}
+}
+
+//if input_1 greater than input_2, switch. Otherwise stay the same
+void compare_2(int index_1, int index_2, unsigned int* input_array, unsigned int* output_array){
+	if(input_array[index_1] > input_array[index_2]){
+		output_array[index_1] = input_array[index_2];
+		output_array[index_2] = input_array[index_1];
+	}
+	else{
+		output_array[index_1] = input_array[index_1];
+		output_array[index_2] = input_array[index_2];
+	}
+}
+}
